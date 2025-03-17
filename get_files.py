0e2b4814f36cb5e793264f5e3ab8bbf1cb9dc6f2
unknown <abhirajshyjuleo2000@gmail.com>
2025-03-17 23:18:03 +0530

    changed files

--- get_files.py
+++ get_files.py
@@ -1,6 +1,6 @@
 import os 
 def get_files():
-    files=os.listdir('C:\\Users\\LENOVO\\Desktop\\New folder (2)\\files\\')
+    files={'extension': {'pdf': {'number of files': 2, 'files': ['file.pdf', 'file4.pdf']}, 'csv': {'number of files': 2, 'files': ['file2.csv', 'file6.csv']}, 'doc': {'number of files': 1, 'files': ['file3.doc']}, 'png': {'number of files': 1, 'files': ['file5.png']}}}
     return files
 
 
